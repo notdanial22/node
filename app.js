9b371073b36f8e4a7ae32157c1f67ec7a230b976
notdanial22 <142971235+notdanial22@users.noreply.github.com>
2023-10-27 16:42:07 -0700

    done some changes for server

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const http = require('http')
 
-const server = http.createServer((req , res)=>{
- if (req.url === '/'){
-  res.end('Welcome  to our home page!')
- }
- if (req.url === '/about'){
-  res.end('This is about page')
- }
-  res.end(`<h1>Ooops!!</h1><p>this page is not available</p><a href ='/'>Home</a>`)
-
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Welcome to our home page!')
+    res.end()
+  } else if (req.url === '/about') {
+    res.write('This is about page')
+    res.end()
+  } else {
+    res.write(
+      `<h1>Ooops!!</h1><p>this page is not available</p><a href ='/'>Home</a>`
+    )
+    res.end()
+  }
 })
 server.listen(5000)
 
-
 const lodash = require('lodash')
 
-const items = [1,[2,[3,4,[5]]]];
+const items = [1, [2, [3, 4, [5]]]]
 const newitems = lodash.flattenDeep(items)
-console.log(newitems);
+console.log(newitems)
